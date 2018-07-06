8ca7ea334d480337a4bf067c7eca6ef3413b86be
kiaransalee <seuakoatl@list.ru>
2018-07-06 15:38:20 +1000

    update

--- homework.php
+++ homework.php
@@ -1,4 +1,6 @@
-<?php
+﻿<?php
+
+// задаем переменные и массивы
 $continents = ["Africa" => ["Tragelaphus strepsiceros","Struthio camelus"],"Antarctica"=>["Belgica antarctica","Euphausia superba"],"Asia"=>["Viverra zibetha","Lipotes vexillifer"],"Australia"=>["Dromaius novaehollandiae","Alectura lathami"],"Europe"=>["Alopex lagopus","Canis aureus"],"North America"=>["Rangifer tarandus","Lynx canadensis"],"South America"=>["Dasyprocta","Inia geoffrensis"]];
 $double_animals=[];
 $double_animals_continents=[];
@@ -7,80 +9,48 @@ $first_parts=[];
 $second_parts=[];
 $fantasy_animals=[];
 $results=[];
-// тут мы задали переменные и массивы
 
-
+//отделяем животных с названием из двух слов
 foreach ($continents as $key1=>$animals) {
     foreach ($animals as $animal) {
         if (strpos($animal, ' ') !== false) {
             $double_animals_continents[$key1][] = $animal;
-
         }
     }
 }
 
-//  тут мы отделили животных с названием из двух слов
-
-//print_r($double_animals);
-
+//разделяем названия из двух слов на разные массивы
 foreach ($double_animals_continents as $key2=>$double_animals) {
-
-
     foreach ($double_animals as $double_animal) {
-
         list($first_parts_continents[$key2][], $second_parts[]) = explode(' ', $double_animal);
     }
 }
-// здесь мы разделяем названия из двух слов на разные массивы
 
-
+// перемешиваем первые части слов, чтобы они стояли в разном порядке
 foreach ($first_parts_continents as $key5=>$first_part_continent) {
-    shuffle($first_parts_continents[$key5]);
+         shuffle($first_parts_continents[$key5]);
 }
 
-
-
-// здесь мы перемешиваем первые части слов, чтобы они стояли в разном порядке
-
-
-   foreach ($first_parts_continents as $key=>$first_part_continent) {
-
+//здесь я назначила рандомный ключ элементам второй части названия, чтобы вторые части подставлялись рандомно и соединила первые и вторые части в 1 словосочетание
+foreach ($first_parts_continents as $key=>$first_part_continent) {
        foreach ($first_part_continent as $key4 => $first_part) {
-
-
-
                $key6 = mt_rand(0, 12);
                $second_part = $second_parts[$key6];
-
-
-
                $fantasy_animals[$key][] = $first_part . ' ' . $second_part;
-
-
            }
        }
 
-//здесь я назначила рандомный ключ элементам второй части названия, чтобы вторые части подставлялись рандомно и соединила первые и вторые части в 1 словосочетание
-
+//здесь мы выводим континенты заголовками и добавляем запятую к названиям животных.
 foreach ($fantasy_animals as $key=>$first_part_continent) {
- 
-
-    foreach ($first_part_continent as $key=>$result) {
-
-        $results[$key][] =  $result . ",";
-
+        foreach ($first_part_continent as $key=>$result) {
+                  $results[$key][] =  $result . ",";
     }
 }
 
-//здесь мы выводим континенты заголовками и добавляем запятую к названиям животных.
-
+//соединияем животных каждого континента в 1 строку и выводим
 foreach ($fantasy_animals as $key=>$first_part_continent) {
-    echo "<h2>" . $key . "</h2>";
-
-    echo implode(', ', $first_part_continent);
-
+         echo "<h2>" . $key . "</h2>";
+         echo implode(', ', $first_part_continent);
 }
-
-   //соединияем животных каждого континента в 1 строку и выводим
 
 ?>
